abe6853e17f7b71fb7fae16a4a4bcc2148f29b7e
chowfi <fc1132+github@nyu.edu>
2026-02-20 17:20:32 -0500

    completed rust implementation of tictactoe

--- tictactoe.rs
+++ tictactoe.rs
@@ -27,8 +27,8 @@ fn display_board(board: [[char; 3]; 3]) {
 }
 
 fn is_valid_move(board: [[char; 3]; 3], row: usize, col: usize) -> bool {
-    if row <= 2 && row >= 0 {
-        if col <= 2 && col >= 0 {
+    if row <= 2  {
+        if col <= 2  {
             if board[row][col] == '0' {
                 return true;
             }
@@ -86,35 +86,41 @@ fn get_player_move(player: char) -> (usize, usize) {
     let col: usize = parts[1].trim().parse().expect("invalid col");
     (row, col)
 }
+
 fn play_game() {
-    player = '1'
-    board = create_empty_board()
-    display_board(board)
-    while not is_board_full(board):
-        if player == '1':
-            while True:
-            # if player == '1':
-                row, col = get_player_move(player)
-                if is_valid_move(board, row, col):
-                    break
-                print(f'Input move invalid. Check the following and try again:\n 1) row and col are within bounds (0-2)\n 2) The target square is empty')
-        else: 
-            row, col = get_random_move(board)
-        board = make_move(board, row, col, player)
-        display_board(board)
-        if check_winner(board) is not None:
-            print(f'Winner: Player {check_winner(board)}')
-            break
-        else:
-            if player == '1':
-                player = '2'
-            elif player == '2':
-                player = '1'
-    if check_winner(board) is None:
-        print (f"Game Ended: It's a cat's game!\n https://english.stackexchange.com/questions/155621/why-is-a-tie-in-tic-tac-toe-called-a-cats-game")
+    let mut player = '1';
+    let mut board = create_empty_board();
+    display_board(board);
+    while !is_board_full(board) {
+        let row;
+        let col;
+        loop {
+            let (r, c) = get_player_move(player);
+            if is_valid_move(board, r, c) {
+                row = r;
+                col = c;
+                break;
+            }
+            println!("Input move invalid. Check the following and try again:\n 1) row and col are within bounds (0-2)\n 2) The target square is empty");
+        }
+        board = make_move(board, row, col, player);
+        display_board(board);
+        if let Some(winner) = check_winner(board) {
+            println!("Winner: Player {}", winner);
+            break;
+        } else {
+            if player == '1' {
+                player = '2';
+            } else if player == '2' {
+                player = '1';
+            }
+        }
+    }
+    if check_winner(board).is_none() {
+        println!("Game Ended: It's a cat's game!\n https://english.stackexchange.com/questions/155621/why-is-a-tie-in-tic-tac-toe-called-a-cats-game");
+    }
 }
 
 fn main() {
-    let board = create_empty_board();
-    display_board(board);
+    play_game();
 }
