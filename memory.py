a2f13b9641ce15496e7061807f20e79ef146ec1d
chowfi <fc1132+github@nyu.edu>
2026-02-27 12:57:54 -0500

    playable memory game

--- memory.py
+++ memory.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 import random
+import time
 
 #Memory in CLI (clickable + AI Agent + color)
 """
@@ -65,7 +66,7 @@ class Memory:
         for r in range(self.row):
             for c in range(self.col):
                 if self.visibility_board[r][c] == True:
-                    self.user_board[r][c] == self.ground_truth[r][c]
+                    self.user_board[r][c] = self.ground_truth[r][c]
 
         for row in self.user_board:
             print(" ".join(row))
@@ -75,9 +76,12 @@ class Memory:
         #start w basic index user input first
         self.temp = []
         for _ in range(2):
-            row, col = input("Input row,col. It's 0th indexed")
+            answer = input("Input row,col. It's 0th indexed")
+            row, col = answer.split(",")
+            row = int(row)
+            col = int(col)
             self.temp.append((row,col))
-
+        print(f'user_temp:{self.temp}')
         # advance to enabling mouse clicking: mouse_y, mouse_x, top, left, height, width
     
     def computer_input(self):
@@ -86,49 +90,80 @@ class Memory:
         while True:
             r, c = random.randint(0,1), random.randint(0,2)
             if self.visibility_board[r][c] == False and count <=1:
-                self.computer_temp.append(r,c)
-                count+=1
+                if len(self.computer_temp) == 1:
+                    if (r,c) not in self.computer_temp:
+                        self.computer_temp.append((r,c))
+                        count+=1
+                else:   
+                    self.computer_temp.append((r,c))
+                    count+=1
             if count == 2:
                 break
+        print(f'computer_temp:{self.computer_temp}')
             
     def check_human_input(self):
         row_0, col_0 = self.temp[0]
         row_1, col_1 = self.temp[1]
-        if self.temp[0][0] >= 0 and self.temp[1][0] >= 0 and self.temp[0][0] < len(self.row) and self.temp[1][0] < len(self.row):
-            if self.temp[0][1] >= 0 and self.temp[1][1] >= 0 and self.temp[0][1] < len(self.col) and self.temp[1][1] < len(self.col):
-                if self.visibillity_board[row_0][col_0] == False and self.visibillity_board[row_1][col_1] == False:
-                    return False
-        return True
+        if self.temp[0][0] >= 0 and self.temp[1][0] >= 0 and self.temp[0][0] < self.row and self.temp[1][0] < self.row:
+            if self.temp[0][1] >= 0 and self.temp[1][1] >= 0 and self.temp[0][1] < self.col and self.temp[1][1] < self.col:
+                if self.visibility_board[row_0][col_0] == False and self.visibility_board[row_1][col_1] == False and (row_0,col_0) != (row_1,col_1):
+                    return True
+        return False
         
     def evaluate_input(self, player, temporary_list):
-        if temporary_list[0] == temporary_list[1]:
-            row_0, col_0 = temporary_list[0]
-            row_1, col_1 = temporary_list[1]
+        row_0, col_0 = temporary_list[0]
+        row_1, col_1 = temporary_list[1]
+        if self.ground_truth[row_0][col_0] == self.ground_truth[row_1][col_1]:
             self.visibility_board[row_0][col_0] = True
             self.visibility_board[row_1][col_1] = True
             self.winning_sets[player] +=1
+            print(f'player {player} just won 1 set')
+        
+        else:
+            print(f'player {player} did not pick a winning set')
 
         temporary_list = []
 
     def is_game_over(self):
         for r in range(self.row):
             for c in range(self.col):
-                if self.visibility_board == False:
+                if self.visibility_board[r][c] == False:
                     return False
         return True
 
     def game_play(self, first_player: str = '1', player_type: str = 'human'):
         curr_player = first_player
-        visualize_user_board()
-        while not is_game_over():
-            while True:
-                if player_type == 'human':
-                    user_input() 
-                    if check_human_input():
+        self.visualize_user_board()
+        while not self.is_game_over():
+            print(f'curr_player: {curr_player} type:{player_type}')
+            if player_type == 'human':
+                while True:
+                    self.user_input() 
+                    if self.check_human_input():
                         break
-
+                    print(f'Input move invalid. Check that row and col are within bounds. The two moves are distinct and unopened.')
+                temporary_list = self.temp
+            if player_type != 'human':
+                self.computer_input()
+                temporary_list = self.computer_temp
+            self.evaluate_input(curr_player, temporary_list)
+            print(f'Sets won: Player 1 - {self.winning_sets['1']}, Player 2 - {self.winning_sets['2']}')
+            self.visualize_user_board()
+            if player_type == 'human':
+                player_type = 'computer'
+            elif player_type == 'computer':
+                player_type = 'human'
+            if curr_player == '1':
+                curr_player = '2'
+            elif curr_player == '2':
+                curr_player = '1'
+            time.sleep(5)
+        largest_key = sorted(self.winning_sets.items(), key=lambda item:item[1], reverse=True)[0][0]
+        largest_val = sorted(self.winning_sets.items(), key=lambda item:item[1], reverse=True)[0][1]
+        print(f'Game ended.')
+        print(f'Winner is {largest_key} with {largest_val} sets won out of {self.col} total sets')
 
 if __name__ == "__main__":
     mem = Memory()
     mem.visualize_ground_truth() #for testing
-    game_play()
+    mem.game_play()
